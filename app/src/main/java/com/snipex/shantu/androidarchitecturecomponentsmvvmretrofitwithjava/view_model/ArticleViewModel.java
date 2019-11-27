c4a9b9fe8494df3be93d1cb90697fbfd9af8235c
Suyog Dorlikar <42290206+suyogdorlikar22@users.noreply.github.com>
2019-11-27 14:41:01 +0530

    Update ArticleViewModel.java

--- app/src/main/java/com/snipex/shantu/androidarchitecturecomponentsmvvmretrofitwithjava/view_model/ArticleViewModel.java
+++ app/src/main/java/com/snipex/shantu/androidarchitecturecomponentsmvvmretrofitwithjava/view_model/ArticleViewModel.java
@@ -12,15 +12,22 @@ public class ArticleViewModel extends AndroidViewModel {
 
     private ArticleRepository articleRepository;
     private LiveData<ArticleResponse> articleResponseLiveData;
+     private MutableLiveData<Boolean> mIsUpdating = new MutableLiveData<>();
+
 
     public ArticleViewModel(@NonNull Application application) {
         super(application);
 
         articleRepository = new ArticleRepository();
+           this.mIsUpdating = postRepository.getUpdating();
         this.articleResponseLiveData = articleRepository.getMovieArticles("movies", "84a7decf3110498ea372bd16dd0601e8");
     }
 
     public LiveData<ArticleResponse> getArticleResponseLiveData() {
         return articleResponseLiveData;
+    }
+    
+        public LiveData<Boolean> getIsUpdating() {
+        return mIsUpdating;
     }
 }
