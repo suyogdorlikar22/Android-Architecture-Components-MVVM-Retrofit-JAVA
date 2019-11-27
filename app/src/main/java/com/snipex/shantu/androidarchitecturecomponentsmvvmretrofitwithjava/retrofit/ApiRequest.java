05bdc0721f330ccd2d7b4a0ea72d3604a8cfffdc
Suyog Dorlikar <42290206+suyogdorlikar22@users.noreply.github.com>
2019-11-27 15:18:56 +0530

    Update ApiRequest.java

--- app/src/main/java/com/snipex/shantu/androidarchitecturecomponentsmvvmretrofitwithjava/retrofit/ApiRequest.java
+++ app/src/main/java/com/snipex/shantu/androidarchitecturecomponentsmvvmretrofitwithjava/retrofit/ApiRequest.java
@@ -18,7 +18,7 @@ public interface ApiRequest {
     
         //======RxJava ==================
 //    @GET("v2/everything/")
-//    Flowable<ArticleResponse> getMovieArticles(
+//    Flowable<List<ArticleResponse>> getMovieArticles(
 //            @Query("q") String query,
 //            @Query("apikey") String apiKey
 //    );
